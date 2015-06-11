4ed40593c182ac5156f5e62913c26948ab24991e
chabrowa <chabrowa@leniak>
2015-06-11 12:22:37 +0100

    basic selection view

--- Prosel/client/controllers/tobedeleted.js
+++ Prosel/client/controllers/tobedeleted.js
@@ -2,8 +2,7 @@ Template.wzornik.events({
   'click #menu-toggle': function(e) {
     e.preventDefault();
     $('#personal-information').toggleClass('col-0 col-md-2');
-    $('#basic-container').toggleClass('col-md-9 col-md-8');
-    $('#help-container-new').toggleClass('col-md-3 col-md-2');
+    $('#basic-container').toggleClass('col-md-9 col-md-7');
   },
   'click #part-function': function(e) {
     Session.set('selectionPage', 'first');
